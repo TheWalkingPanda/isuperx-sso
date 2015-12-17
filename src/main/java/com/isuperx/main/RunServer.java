fbe071323da0e8d1c4ee55b248219c3366365efd
bingbing.lao <bingbing.lao@TSJ0465.sky-mobi.com>
2015-12-17 18:08:15 +0800

    update

--- src/main/java/com/isuperx/main/RunServer.java
+++ src/main/java/com/isuperx/main/RunServer.java
@@ -11,7 +11,7 @@ public class RunServer {
 	
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
-		context.start();//完成bean对象的初始化工作 
+		context.start();//完成bean对象的初始化工作
 		log.info("isperx-sso服务启动成功 : ");
 		log.info("输入任意字符停止服务 : ");
 		try {
